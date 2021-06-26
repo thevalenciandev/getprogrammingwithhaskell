9d3ec5abe5c5153d40f96dc6cc4ab7f6355cdae5
thevalenciandev <thevalenciandev@gmail.com>
2021-06-26 16:28:18 +0100

    Robot def and fn

---
+++ lesson10/robot.hs
@@ -0,0 +1,15 @@
+killaRobot = robot ("Kill@",25,2000)
+
+robot (name,attack,hitpoints) = \fn -> fn (name,attack,hitpoints)
+
+name (n,_,_) = n
+attack (_,a,_) = a
+hitpoints (_,_,hp) = hp
+
+getName aRobot   = aRobot name
+getAttack aRobot = aRobot attack
+getHitPoints aRobot = aRobot hitpoints
+
+setName aRobot newName     = aRobot (\(n,a,h) -> robot (newName,a,h))
+setAttack aRobot newAttack = aRobot (\(n,a,h) -> robot (n,newAttack,h))
+setHitPoints aRobot newHP  = aRobot (\(n,a,h) -> robot (n,a,newHP))
