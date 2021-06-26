eabcdd9b77f1b510b1ae15e7df5157897422bc13
thevalenciandev <thevalenciandev@gmail.com>
2021-06-26 15:19:57 +0100

    isEmpty fn, some comments and types

--- lesson10/cup.hs
+++ lesson10/cup.hs
@@ -1,3 +1,8 @@
+-- An example cup of coffee just for playing on GHCI
+cuppa = cup 5
+
+-- Coffee functions
+cup :: Num a => a -> (b -> a)
 cup flOz = \_ -> flOz
 
 getOz aCup = aCup (\flOz -> flOz)
@@ -5,3 +10,5 @@ getOz aCup = aCup (\flOz -> flOz)
 drink aCup ozDrank = let oz = getOz aCup
                          leftoz = oz - ozDrank
                      in if leftoz <= 0 then cup 0 else cup leftoz
+
+isEmpty aCup = getOz aCup == 0
