60a4eb4e4a5a174648e42ef151e58fe01c5a83a7
thevalenciandev <thevalenciandev@gmail.com>
2021-06-26 16:43:10 +0100

    Fight fn

--- lesson10/robot.hs
+++ lesson10/robot.hs
@@ -1,4 +1,5 @@
-killaRobot = robot ("Kill@",25,2000)
+killaRobot = robot ("Kill@",25,200)
+gentlGiant = robot ("Gentl",10,300)
 
 robot (name,attack,hitpoints) = \fn -> fn (name,attack,hitpoints)
 
@@ -16,4 +17,12 @@ setHitPoints aRobot newHP  = aRobot (\(n,a,h) -> robot (n,a,newHP))
 
 printRobot aRobot = aRobot (\(n,a,h) -> n ++ " attack: " ++ (show a) ++ " hp: " ++ (show h))
 
+-- Returns a new instance of the robot with the damage substracted
 damage aRobot attackDamage = aRobot (\(n,a,h) -> robot (n,a,h-attackDamage))
+
+-- Returns the defender robot with the damage applied to it
+-- if attacker (aRobot) has no life, then it won't attack
+fight aRobot defender = damage defender attack
+   where attack = if getHitPoints aRobot > 10
+                  then getAttack aRobot
+                  else 0
