76977bf8c39c7241e522ca34282ac10759daee2f
thevalenciandev <thevalenciandev@gmail.com>
2021-06-27 18:53:46 +0100

    Remove boiler plate with default Eq and Ord impl.

--- lesson14/sixsideddie.hs
+++ lesson14/sixsideddie.hs
@@ -1,4 +1,4 @@
-data SixSidedDie = S1 | S2 | S3 | S4 | S5 | S6
+data SixSidedDie = S1 | S2 | S3 | S4 | S5 | S6 deriving (Eq, Ord)
 
 instance Show SixSidedDie where
   -- show :: a -> String
@@ -8,32 +8,3 @@ instance Show SixSidedDie where
   show S4 = "four"
   show S5 = "five"
   show S6 = "six"
-
-instance Eq SixSidedDie where
-  -- (==) :: a -> a -> Bool
-  (==) S6 S6 = True
-  (==) S5 S5 = True
-  (==) S4 S4 = True
-  (==) S3 S3 = True
-  (==) S2 S2 = True
-  (==) S1 S1 = True
-  (==) _ _ = False
-
-instance Ord SixSidedDie where
-  -- compare :: a -> a -> Bool
-  compare S1 S1 = EQ
-  compare S1 _  = LT
-  compare _ S1  = GT
-  compare S2 S2 = EQ
-  compare S2 _  = LT
-  compare _ S2  = GT
-  compare S3 S3 = EQ
-  compare S3 _  = LT
-  compare _ S3  = GT
-  compare S4 S4 = EQ
-  compare S4 _  = LT
-  compare _ S4  = GT
-  compare S5 S5 = EQ
-  compare S5 _  = LT
-  compare _ S5  = GT
-  compare S6 S6 = EQ
