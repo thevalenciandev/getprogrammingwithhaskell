a923b100dd8c250b0c73707765ca5a7202bc5bfa
thevalenciandev <thevalenciandev@gmail.com>
2021-06-28 06:28:35 +0100

    Deriving Enum to fix bug on missing end value

--- lesson14/sixsideddie.hs
+++ lesson14/sixsideddie.hs
@@ -1,4 +1,4 @@
-data SixSidedDie = S1 | S2 | S3 | S4 | S5 | S6 deriving (Eq, Ord)
+data SixSidedDie = S1 | S2 | S3 | S4 | S5 | S6 deriving (Eq, Ord, Enum)
 
 instance Show SixSidedDie where
   -- show :: a -> String
@@ -8,20 +8,3 @@ instance Show SixSidedDie where
   show S4 = "four"
   show S5 = "five"
   show S6 = "six"
-
-instance Enum SixSidedDie where
-  -- toEnum :: Int -> SixSidedSide
-  toEnum 0 = S1
-  toEnum 1 = S2
-  toEnum 2 = S3
-  toEnum 3 = S4
-  toEnum 4 = S5
-  toEnum 5 = S6
-  toEnum _ = error "No such value"
-  -- fromEnum :: SixSidedSide -> Int
-  fromEnum S1 = 0
-  fromEnum S2 = 1
-  fromEnum S3 = 2
-  fromEnum S4 = 3
-  fromEnum S5 = 4
-  fromEnum S6 = 5
