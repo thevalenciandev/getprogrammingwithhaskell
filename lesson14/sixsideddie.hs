c780dca98280be95252e752e2790fd8fc7320061
thevalenciandev <thevalenciandev@gmail.com>
2021-06-28 06:27:01 +0100

    Implementing Enum to be able to enumerate on SixSidedDie

--- lesson14/sixsideddie.hs
+++ lesson14/sixsideddie.hs
@@ -8,3 +8,20 @@ instance Show SixSidedDie where
   show S4 = "four"
   show S5 = "five"
   show S6 = "six"
+
+instance Enum SixSidedDie where
+  -- toEnum :: Int -> SixSidedSide
+  toEnum 0 = S1
+  toEnum 1 = S2
+  toEnum 2 = S3
+  toEnum 3 = S4
+  toEnum 4 = S5
+  toEnum 5 = S6
+  toEnum _ = error "No such value"
+  -- fromEnum :: SixSidedSide -> Int
+  fromEnum S1 = 0
+  fromEnum S2 = 1
+  fromEnum S3 = 2
+  fromEnum S4 = 3
+  fromEnum S5 = 4
+  fromEnum S6 = 5
