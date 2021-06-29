c1162cb2974e252a701d4a1e3d2eeddf43e7ddc2
thevalenciandev <thevalenciandev@gmail.com>
2021-06-29 06:53:29 +0100

    rotN fn

---
+++ lesson15/rot.hs
@@ -0,0 +1,7 @@
+data FourLetterAlphabet = L1 | L2 | L3 | L4 deriving (Show, Enum, Bounded)
+
+rotN :: (Bounded a, Enum a) => Int -> a -> a
+rotN n letter = let middle = n `div` 2
+                    letter' = fromEnum letter
+                    offset = (letter' + middle) `mod` n
+                in toEnum offset
