03984bfbec03065290f2062832b5b46b3072ec34
thevalenciandev <thevalenciandev@gmail.com>
2021-06-30 20:32:00 +0100

    Adding Bits functions

---
+++ lesson15/bits.hs
@@ -0,0 +1,17 @@
+type Bits = [Bool]
+
+intToBits' :: Int -> Bits
+intToBits' 0 = [False]
+intToBits' 1 = [True]
+intToBits' n = let next = n `div` 2
+                   remainder = n `mod` 2
+                   bit = if remainder == 0 then False else True
+               in bit : intToBits' next
+
+maxBits :: Int
+maxBits = length $ intToBits' maxBound
+
+intToBits :: Int -> Bits
+intToBits n = replicate padding False ++ reversed 
+    where reversed = reverse $ intToBits' n
+          padding = maxBits - (length reversed)
