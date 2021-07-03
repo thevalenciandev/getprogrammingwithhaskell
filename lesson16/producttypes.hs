ae5fdf936b46b49beb89fdb07628832d322e6bb4
thevalenciandev <thevalenciandev@gmail.com>
2021-07-03 18:32:42 +0100

    Designing Creators using sum types
    Designing Creators using sum types

--- lesson16/producttypes.hs
+++ lesson16/producttypes.hs
@@ -1,12 +1,22 @@
-data AuthorName = AuthorName {
-    name    :: String
-  , surname :: String
-} deriving Show
+type FirstName = String
+type LastName  = String
+type MiddleName = String
+
+data Name = Name FirstName LastName | NameWithMiddle FirstName MiddleName LastName deriving Show
 
 data Book = Book {
-    author :: AuthorName
+    author :: Name
   , isbn   :: String
   , title  :: String
   , year   :: Int
   , price  :: Double
 } deriving Show
+
+data Author = Author Name deriving Show
+data Artist = Person Name | Band String deriving Show
+
+data Creator = AuthorCreator Author | ArtistCreator Artist deriving Show
+
+-- Some dummy data
+c1 = AuthorCreator $ Author $ Name "John" "Salchichon"
+c2 = AuthorCreator $ Author $ NameWithMiddle "John" "Salchichon" "Rambo"
