3c69ecd25eb96bab5f2e2b88064ec5fac7e911b3
thevalenciandev <thevalenciandev@gmail.com>
2021-07-04 17:03:24 +0100

    Adding StoreItems

--- lesson16/producttypes.hs
+++ lesson16/producttypes.hs
@@ -6,6 +6,7 @@ data Name = Name FirstName LastName | NameWithMiddle FirstName MiddleName LastNa
 
 data Author = Author Name deriving Show
 data Artist = Person Name | Band String deriving Show
+data Creator = AuthorCreator Author | ArtistCreator Artist deriving Show
 
 data Book = Book {
     author :: Creator
@@ -20,11 +21,18 @@ data VinylRecord = VinylRecord {
   , recordTitle  :: String -- can't use title: conflicts with the Book one
   , recordYear   :: Int    -- can't use year: conflicts with the Book one
   , recordPrice  :: Double -- can't use price: conflicts with the Book one
-}
+} deriving Show
 
-data Creator = AuthorCreator Author | ArtistCreator Artist deriving Show
+data CollectibleToy = CollectibleToy {
+    name :: String
+  , description :: String
+  , toyPrice :: Double
+} deriving Show
+
+data StoreItem = BookItem Book | RecordItem VinylRecord | ToyItem CollectibleToy deriving Show
 
 -- Some dummy data
 c1 = AuthorCreator $ Author $ Name "John" "Salchichon"
 c2 = AuthorCreator $ Author $ NameWithMiddle "John" "Salchichon" "Rambo"
 c3 = ArtistCreator $ Band "Metallica"
+i1 = BookItem (Book {author = c1, isbn = "ISBN123", title = "War stories", year = 1984, price = 4.99})
