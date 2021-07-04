e34f167a3a85d1e4f103628c644cc50838577ff6
thevalenciandev <thevalenciandev@gmail.com>
2021-07-04 17:11:58 +0100

    Adding generic price fn for StoreItems

--- lesson16/producttypes.hs
+++ lesson16/producttypes.hs
@@ -13,7 +13,7 @@ data Book = Book {
   , isbn   :: String
   , title  :: String
   , year   :: Int
-  , price  :: Double
+  , bookPrice  :: Double
 } deriving Show
 
 data VinylRecord = VinylRecord {
@@ -31,8 +31,14 @@ data CollectibleToy = CollectibleToy {
 
 data StoreItem = BookItem Book | RecordItem VinylRecord | ToyItem CollectibleToy deriving Show
 
+price :: StoreItem -> Double
+price (BookItem b)   = bookPrice b
+price (RecordItem r) = recordPrice r
+price (ToyItem t)    = toyPrice t
+
 -- Some dummy data
 c1 = AuthorCreator $ Author $ Name "John" "Salchichon"
 c2 = AuthorCreator $ Author $ NameWithMiddle "John" "Salchichon" "Rambo"
 c3 = ArtistCreator $ Band "Metallica"
-i1 = BookItem (Book {author = c1, isbn = "ISBN123", title = "War stories", year = 1984, price = 4.99})
+i1 = BookItem (Book {author = c1, isbn = "ISBN123", title = "War stories", year = 1984, bookPrice = 4.99})
+i2 = RecordItem (VinylRecord {artist = c3, recordTitle = "Garage Days Re0Revisited", recordYear = 1987, recordPrice = 7.99})
