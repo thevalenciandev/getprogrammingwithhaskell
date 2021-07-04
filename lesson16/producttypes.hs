d34d280f68751c217d8d817b84af1b6bd9707a58
thevalenciandev <thevalenciandev@gmail.com>
2021-07-04 14:14:09 +0100

    Refactor to use Creator

--- lesson16/producttypes.hs
+++ lesson16/producttypes.hs
@@ -4,19 +4,27 @@ type MiddleName = String
 
 data Name = Name FirstName LastName | NameWithMiddle FirstName MiddleName LastName deriving Show
 
+data Author = Author Name deriving Show
+data Artist = Person Name | Band String deriving Show
+
 data Book = Book {
-    author :: Name
+    author :: Creator
   , isbn   :: String
   , title  :: String
   , year   :: Int
   , price  :: Double
 } deriving Show
 
-data Author = Author Name deriving Show
-data Artist = Person Name | Band String deriving Show
+data VinylRecord = VinylRecord {
+    artist :: Creator
+  , recordTitle  :: String -- can't use title: conflicts with the Book one
+  , recordYear   :: Int    -- can't use year: conflicts with the Book one
+  , recordPrice  :: Double -- can't use price: conflicts with the Book one
+}
 
 data Creator = AuthorCreator Author | ArtistCreator Artist deriving Show
 
 -- Some dummy data
 c1 = AuthorCreator $ Author $ Name "John" "Salchichon"
 c2 = AuthorCreator $ Author $ NameWithMiddle "John" "Salchichon" "Rambo"
+c3 = ArtistCreator $ Band "Metallica"
